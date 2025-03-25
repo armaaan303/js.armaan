d692b262a227cbc1b4800255007acac1f51723ec
armaaan303 <armaannoor0181@gmail.com>
2025-03-25 00:44:05 +0000

    variables and datatypes

---
+++ 01-basic/01variables.js
@@ -0,0 +1,11 @@
+const accountid=121212
+let accountemail="hitesh@gmail.com"
+var accountpass="123"
+accountcity="jaipur"
+
+accountemail="ah@gmail.com"
+accountpass="1"
+accountcity="punjb"
+
+console.log(accountid);
+console.table([accountemail,accountid,accountpass,accountcity])
---
+++ 02-basic/datatypes.js
@@ -0,0 +1,9 @@
+console.log("amaan")
+let name="sanjana"
+let age =18
+console.log(typeof "name")
+console.log(typeof age)
+let date=null;
+console.log(typeof null)
+let cast
+console.log(typeof cast)
